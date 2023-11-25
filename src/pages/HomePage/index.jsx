c4fc90eb6c788f1a455b63d329bff06bca4e8fc9
Karolína Mukařovská <k.mukarovska01@gmail.com>
2023-11-25 11:21:06 +0100

    json data fetch, props have been set

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,10 +1,35 @@
 import { Joke } from '../../Components/Joke';
 import './style.css';
+import { useState, useEffect } from 'react';
 
 export const HomePage = () => {
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {
+    const handleFetch = async () => {
+      const response = await fetch('http://localhost:4000/api/jokes');
+      const data = await response.json();
+      setJokes(data.result);
+      console.log(data.result);
+    };
+
+    handleFetch();
+  }, []);
+
   return (
     <div className="container">
-      <Joke />
+      {jokes.map((joke) => {
+        return (
+          <Joke
+            key={joke.id}
+            userAvatar={joke.avatar}
+            userName={joke.name}
+            text={joke.text}
+            likes={joke.likes}
+            dislikes={joke.dislikes}
+          />
+        );
+      })}
     </div>
   );
 };
