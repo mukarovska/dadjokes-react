eabacf050e300dd4c0f84ae189dd88ed834847d6
Karolína Mukařovská <k.mukarovska01@gmail.com>
2023-11-25 11:10:50 +0100

    jokes.json added, local api started

--- src/Components/Joke/index.jsx
+++ src/Components/Joke/index.jsx
@@ -1,26 +1,19 @@
 import { JokeLikes } from '../JokeLikes';
 import './style.css';
 
-export const Joke = () => {
+export const Joke = ({ userAvatar, userName, text, likes, dislikes }) => {
   return (
     <div className="joke">
       <div className="joke__body">
         <div className="joke__user">
-          <img
-            className="user-avatar"
-            src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
-          />
-          <p className="user-name">Neroxx</p>
+          <img className="user-avatar" src={userAvatar} />
+          <p className="user-name">{userName}</p>
         </div>
 
-        <p className="joke__text">
-          The secret service isn't allowed to yell "Get down!" anymore when the
-          president is about to be attacked. Now they have to yell "Donald,
-          duck!"
-        </p>
+        <p className="joke__text">{text}</p>
       </div>
 
-      <JokeLikes />
+      <JokeLikes likes={likes} dislikes={dislikes} />
     </div>
   );
 };
